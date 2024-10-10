cc9a1b417b25d48eab4b3cfbcadef71a4fb3f1a8
mee-rich <Jamessuyi@gmail.com>
2024-10-10 05:46:36 +0100

    done

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -2,43 +2,29 @@
 // Create two students, and create an array named studentsList containing the two variables
 // Using Vanilla Javascript, render a table and for each elements in the array, append a new row to the table
 // Each row should contain the first name of the student and the location
-
-// Define the Student interface
-interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
-}
-
 // Create two student objects
-const firstStudent: Student = {
-	firstName: "Adegbenga",
-	lastName: "Adesuyi",
-	age: 66,
-	location: "Nigeria"
-}
-
-const secondStudent: Student = {
-	firstName: "Titi",
-	lastName: "Adesuyi",
-	age: 56,
-	location: "Nigeria"
-}
-
+var firstStudent = {
+    firstName: "Adegbenga",
+    lastName: "Adesuyi",
+    age: 66,
+    location: "Nigeria"
+};
+var secondStudent = {
+    firstName: "Titi",
+    lastName: "Adesuyi",
+    age: 56,
+    location: "Nigeria"
+};
 // Create an array of students
-const studentsList = [firstStudent, secondStudent];
-
+var studentsList = [firstStudent, secondStudent];
 // Using Vanilla js to create and display the table
-const table = document.createElement('table');
-const headerRow = table.insertRow();
+var table = document.createElement('table');
+var headerRow = table.insertRow();
 headerRow.innerHTML = '<th>First Name</th><th>Locations</th>';
-
 // Create a new row for each student and append it to the table
-studentsList.forEach((student) => {
-	const row = table.insertRow();
-	row.innerHTML = `<td>${student.firstName}</td><td>${student.location}</td>`;
+studentsList.forEach(function (student) {
+    var row = table.insertRow();
+    row.innerHTML = "<td>".concat(student.firstName, "</td><td>").concat(student.location, "</td>");
 });
-
 // Append the table to the body of the HTML document
 document.body.appendChild(table);
